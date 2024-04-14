9bb0d3db21317092702041c6882e5b270d4156b0
hsk <ghdtjd1502@gmail.com>
2024-04-14 17:38:10 +0900

    0414commit

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,7 +1,23 @@
 
-
 plugins {
-    kotlin("plugin.jpa") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23" apply false
+}
+
+
+dependencies {
+        implementation(project(":application:inquire-application"))
+        implementation(project(":application:sync-application"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        runtimeOnly(project(":internal"))
+}
+
+
+allprojects{
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation(project(":domain"))
+    }
+
 }
 
 
@@ -13,20 +29,3 @@ tasks.getByName("bootJar") {
 tasks.getByName("jar") {
     enabled = false
 }
-
-
-dependencies {
-        implementation(project(":application:inquire-application"))
-        implementation(project(":application:sync-application"))
-}
-
-
-allprojects{
-    dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation(project(":domain"))
-        runtimeOnly(project(":internal"))
-    }
-
-}
-
--- internal/build.gradle.kts
+++ internal/build.gradle.kts
@@ -1,10 +1,4 @@
 
-//allOpen{
-//    annotation("javax.persistence.Entity")
-//    annotation("javax.persistence.MappedSuperclass")
-//    annotation("javax.persistence.Embeddable")
-//}
-
 dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     compileOnly(project(":domain"))
