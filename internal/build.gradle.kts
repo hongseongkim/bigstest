2b749d2de44206eac9f71ea849b7f9c173644ea0
hsk <ghdtjd1502@gmail.com>
2024-04-14 17:06:26 +0900

    test

--- internal/build.gradle.kts
+++ internal/build.gradle.kts
@@ -1,15 +1,11 @@
-plugins {
-    kotlin("plugin.jpa") version "1.9.23"
-}
 
-allOpen{
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.MappedSuperclass")
-    annotation("javax.persistence.Embeddable")
-}
+//allOpen{
+//    annotation("javax.persistence.Entity")
+//    annotation("javax.persistence.MappedSuperclass")
+//    annotation("javax.persistence.Embeddable")
+//}
 
 dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     compileOnly(project(":domain"))
 }
